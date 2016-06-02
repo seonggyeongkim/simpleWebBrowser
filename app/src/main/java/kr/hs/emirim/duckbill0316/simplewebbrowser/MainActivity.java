93aa86a6bf6b052cd34c1e15d1babebac12840fb
Sunggyeong Kim <skim7902@gmail.com>
2016-06-02 20:57:27 +0900

    #2 Event Programming (search url)

--- app/src/main/java/kr/hs/emirim/duckbill0316/simplewebbrowser/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/duckbill0316/simplewebbrowser/MainActivity.java
@@ -3,7 +3,9 @@ package kr.hs.emirim.duckbill0316.simplewebbrowser;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -23,7 +25,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         butMove=(Button)findViewById(R.id.but_move);
         butPrev=(Button)findViewById(R.id.but_prev);
         web=(WebView)findViewById(R.id.web);
-        butMove.setOnClickListener(this);
+
+        butMove.setOnClickListener(this); //event handler 연결!!
+        butMove.setOnClickListener(this); // -> 연결을 해줘야! onClick 클래스 사용 가능!!
+
+        web.setWebViewClient(new WebViewClient());
+        WebSettings webSet=web.getSettings();
+        webSet.setBuiltInZoomControls(true);
+        webSet.setJavaScriptEnabled(true);
+        //web.loadUrl("http://www.naver.com");
     }
 
     /**
@@ -33,6 +43,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     @Override
     public void onClick(View v) {
+
+        String url=null;
+
+        switch (v.getId()){
+            case R.id.but_move :
+                url=editUrl.getText().toString();
+                if(!url.contains("http://"))
+                    url="http://"+url;
+                web.loadUrl(url);
+                //web.loadUrl(editUrl.getText().toString())//Stirng 값으로 변환!!
+                break;
+            case R.id.but_prev :
+                web.goBack(); //이전 페이지로 이동!
+                break;
+        }
 
     }
 }
